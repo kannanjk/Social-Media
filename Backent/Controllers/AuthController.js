4581fac2b73cce972ca6b9151f637a8ab46fcd29
kannan <jishnukumbadam@gmail.com>
2023-02-12 20:19:31 +0530

    crud opreation done

--- Backent/Controllers/AuthController.js
+++ Backent/Controllers/AuthController.js
@@ -2,18 +2,41 @@ import UserModel from "../Models/UserModel.js";
 import bcrypt from 'bcrypt'
 
 // Register a new User 
-export const registerUser = async(req,res)=>{
-    
-    const { firstname,lastname,username,password }=req.body
+export const registerUser = async (req, res) => {
+
+    const { firstname, lastname, username, password } = req.body
 
     const salt = await bcrypt.genSalt(10)
-    const hashedpass = await bcrypt.hash(password,salt)
+    const hashedpass = await bcrypt.hash(password, salt)
 
-    const newUser = new UserModel({firstname,lastname,username, password:hashedpass})
-    try {    
+    const newUser = new UserModel({ firstname, lastname, username, password: hashedpass })
+    try {
         await newUser.save()
         res.status(200).json(newUser)
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message })
+    }
+}
+     
+// login user
+export const loginUser = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const user = await UserModel.findOne({ username: username })
+
+        if (user) {
+
+            console.log(user);
+           
+            const validity = await bcrypt.compare(password, user.password)
+           
+            console.log(password);
+            validity? res.status(200).json(user): res.status(404).json("Wrong Password")
+        } else {
+            res.status(404).json("user dose not exist")
+        }
+    } catch (error) {
+       res.status(500).json({message:error.message})
     }
 }
