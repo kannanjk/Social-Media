7c1bb135c245d59d6bf87578af13231cf60c9502
kannan <jishnukumbadam@gmail.com>
2023-02-14 13:24:05 +0530

    deleted post Api done

--- Backent/Controllers/PostController.js
+++ Backent/Controllers/PostController.js
@@ -14,7 +14,7 @@ export const createPost = async (req, res) => {
 }
 
 // Get a Post
-export const getPost = async (req,res)=>{
+export const getPost = async (req, res) => {
     const id = req.params.id
     try {
         const post = await PostModel.findById(id)
@@ -25,16 +25,34 @@ export const getPost = async (req,res)=>{
 }
 
 // Update a Post
-export const updatePost = async(req,res)=>{
+export const updatePost = async (req, res) => {
     const postId = req.params.id
-    const {userId} = req.body
+    const { userId } = req.body
 
     try {
         const post = await PostModel.findById(postId)
         if (post.userId === userId) {
-            await post.updateOne({$set:req.body})
+            await post.updateOne({ $set: req.body })
             res.status(200).json("post Updated")
-        }else{
+        } else {
+            res.status(403).json("Action forbidden")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// Delete a post 
+export const deletePost = async (req, res) => {
+    const id = req.params.id
+    const { userId } = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+        if (post.userId === userId) {
+            await post.deleteOne( )
+            res.status(200).json("post Deleted")
+        } else {
             res.status(403).json("Action forbidden")
         }
     } catch (error) {
